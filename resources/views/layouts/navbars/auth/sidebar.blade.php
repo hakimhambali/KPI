0ba3f8e59087f2e22f83b4f90455f7f0d7f4f5de
hakimhambali <hakimhambali77@gmail.com>
2022-04-06 12:39:39 +0800

    auto login 3

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -204,7 +204,8 @@
 
             @if (Auth::user()->role != "moderator")
             <li class="nav-item">
-                <a class="nav-link {{ Route::currentRouteName() == 'findowner' ? 'active' : '' }}" href="http://findowner.x61tbrxchx-ewx3lmoq56zq.p.runcloud.link/login2?ic_user={{auth()->user()->ic}}">
+                {{-- <a class="nav-link {{ Route::currentRouteName() == 'findowner' ? 'active' : '' }}" href="http://findowner.test/login2?ic_user={{auth()->user()->ic}}"> --}}
+                    <a class="nav-link {{ Route::currentRouteName() == 'findowner' ? 'active' : '' }}" href="http://findowner.x61tbrxchx-ewx3lmoq56zq.p.runcloud.link/loginProcess?ic_user={{auth()->user()->ic}}">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-x" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z"/>
