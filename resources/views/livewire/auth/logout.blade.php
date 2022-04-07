74050c7176114f40cb5cc2dcf3f95231e403aa53
SyazaitulMNR <syazaitulmnr@gmail.com>
2022-04-07 14:33:59 +0800

    logoutbutton_lg

--- resources/views/livewire/auth/logout.blade.php
+++ resources/views/livewire/auth/logout.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <i class="bi bi-door-open-fill me-sm-1 text-dark {{ in_array(request()->route()->getName(),['profile', 'my-profile']) ? 'text-white' : '' }}" wire:click="logout"></i>
+    <i class="bi bi-door-open-fill text-lg me-sm-1 text-dark {{ in_array(request()->route()->getName(),['profile', 'my-profile']) ? 'text-white' : '' }}" wire:click="logout"></i>
     <span class="d-sm-inline d-none text-dark {{ in_array(request()->route()->getName(),['profile', 'my-profile']) ? 'text-white' : '' }}" wire:click="logout">Sign Out</span>
 </div>
 
