c8bd722b1eba6fd327a57934f65cf52941bfe014
hakimhambali <hakimhambali77@gmail.com>
2022-04-11 10:50:56 +0800

    4

--- app/Http/Livewire/KPI.php
+++ app/Http/Livewire/KPI.php
@@ -40,6 +40,7 @@ class KPI extends Component
 
     public function delete()
     {
+        dd('john');
         $date_id = $this->date_id;
         $user_id = $this->user_id;
         $year = $this->year;
